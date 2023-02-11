f7f2c4514e87d3a0f266b16d95bbbfedec175e07
Eric Oliveira <oiericoi@hotmail.com>
2023-02-11 19:17:38 -0300

    feat: add database connection

---
+++ config/connection.php
@@ -0,0 +1,14 @@
+<?php 
+  $host = "localhost";
+  $db = "agenda";
+  $user = "root";
+  $pass = "oiericdiego22";
+
+  try {
+    $connection = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $erro) {
+    $error = $erro->getMessage();
+    echo "Erro: $error";
+  }
+?>
