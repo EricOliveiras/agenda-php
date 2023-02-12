a638ed3e7829c434397530c7e1714545ddefc178
Eric Oliveira <oiericoi@hotmail.com>
2023-02-12 19:19:36 -0300

    refactor: modify index page

--- index.php
+++ index.php
@@ -25,8 +25,12 @@
               <td scope="row"><?php echo $contact["phone"]; ?></td>
               <td class="actions">
                 <a href="<?php echo $BASE_URL ?>show.php?id=<?php echo $contact["id"]; ?>"><i class="fas fa-eye check-icon"></i></a>
-                <a href="#"><i class="far fa-edit edit-icon"></i></a>
-                <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                <a href="<?php echo $BASE_URL ?>edit.php?id=<?php echo $contact["id"]; ?>"><i class="far fa-edit edit-icon"></i></a>
+                <form class="delete-form" action="<?php echo $BASE_URL ?>config/process.php" method="post">
+                  <input type="hidden" name="type" value="delete">
+                  <input type="hidden" name="id" value="<?php echo $contact["id"]; ?>">
+                  <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                </form>
               </td>
             </tr>
           <?php endforeach; ?>
